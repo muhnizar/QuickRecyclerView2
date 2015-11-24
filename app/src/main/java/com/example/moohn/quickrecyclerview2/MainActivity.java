dc3d4d6e0083a3ab6492a19d76be75bb25b93d7a
muhnizar <mooh.nizar@yahoo.com>
2015-11-24 07:13:36 +0700

    +add divider decoration

--- app/src/main/java/com/example/moohn/quickrecyclerview2/MainActivity.java
+++ app/src/main/java/com/example/moohn/quickrecyclerview2/MainActivity.java
@@ -1,16 +1,15 @@
 package com.example.moohn.quickrecyclerview2;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
+
+import com.example.android.supportv7.widget.decorator.DividerItemDecoration;
+
+import jp.wasabeef.recyclerview.animators.SlideInUpAnimator;
 
 public class MainActivity extends AppCompatActivity {
     ContactAdapter adapter;
@@ -31,8 +30,23 @@ public class MainActivity extends AppCompatActivity {
         adapter = new ContactAdapter(Contact.createContactList(20));
 
         rvContacts.setAdapter(adapter);
-        rvContacts.setLayoutManager(new LinearLayoutManager(this) {
-        });
+
+//      display divider between each items.
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST);
+        rvContacts.addItemDecoration(itemDecoration);
+
+        // Setup layout manager for items
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+//      Control orientation of the items
+//      also supports LinearLayoutManager.HORIZONTAL
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+//      Optionally customize the position you want to default scroll to
+        layoutManager.scrollToPosition(0);
+//      Attach layout manager to the RecyclerView
+        rvContacts.setLayoutManager(layoutManager);
+
+
+        rvContacts.setItemAnimator(new SlideInUpAnimator());
 
 //        add more contact listener
         Button addMoreButton = (Button) findViewById(R.id.add_more_contacts);
